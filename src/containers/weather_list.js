943226b4b5a92a31a23123d817ab32129d2a42e6
Joseph Reinhart <joseph.reinhart@absorblms.com>
2018-02-23 14:22:59 -0700

    did celcius maphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,7 +6,7 @@ import GoogleMap from '../components/google_map';
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp-273);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         const { lon, lat } = cityData.city.coord;
@@ -16,7 +16,7 @@ class WeatherList extends Component {
         return (
             <tr key={name}>
                 <td><GoogleMap lon={lon} lat={lat}/></td>
-                <td><Chart data={temps} color="blue" units="k"/></td>
+                <td><Chart data={temps} color="blue" units="C"/></td>
                 <td><Chart data={pressures} color="red" units="hPa"/></td>
                 <td><Chart data={humidities} color="orange" units="%"/></td>
             </tr>
